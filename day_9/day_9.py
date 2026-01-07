119437b1db1d747c4eaf86b16f4a0b6059dbdc3f
Grady <grady.w.georgia@gmail.com>
2026-01-07 15:59:53 -0600

    maybe possibly an idea for day 9 part 2 :|

--- day_9/day_9.py
+++ day_9/day_9.py
@@ -1,32 +1,67 @@
 import numpy as np
+from enum import Enum, auto
+
+class Direction(Enum):
+    UP    = auto()
+    LEFT  = auto()
+    DOWN  = auto()
+    RIGHT = auto()
+
+class DirectionalLine:
+    def __init__(self, coord1, coord2, direction):
+        self.coord1 = coord1
+        self.coord2 = coord2
+        self.direction = direction
 
 def main():
     with open("./input.txt") as f:
         input = f.read().strip()
-    pairs = [[int(x) for x in line.split(",")] for line in input.split("\n")]
+    coords = [[int(x) for x in line.split(",")] for line in input.split("\n")]
+
+    lines = coords_to_lines(coords)
+    directional_lines = get_directional_lines(lines)
+    h_ranges = get_h_ranges(directional_lines)
+    
+    t_lines = coords_to_lines(transpose_coords(coords))
+    t_directional_lines = get_directional_lines(t_lines)
+    v_ranges = get_h_ranges(t_directional_lines)
 
     res = 0
-    for i in range(len(pairs)):
-        for j in range(i + 1, len(pairs)):
-            if is_valid(pairs[i], pairs[j], pairs):
-                res = max(res, area(pairs[i], pairs[j]))
+    for i in range(len(coords)):
+        for j in range(i + 1, len(coords)):
+            if is_valid(coords[i], coords[j], h_ranges, v_ranges):
+                res = max(res, area(coords[i], coords[j]))
     print(res)
+
+#hranges[y]->[(x1,x2)...]
+#vranges[x]->[(y1,y2)...]
+def get_h_ranges(directional_lines):
+    pass
+
+def transpose_coords(coords):
+    new_coords = []
+    for coord in coords:
+        new_coords.append(coord[1], coord[0])
+
+    return new_coords
+
+def coords_to_lines(coords):
+    lines = []
+    for i in range(len(coords) - 1):
+        lines.append((coords[i], coords[i + 1]))
+    lines.append(coords[-1], coords[0])
+
+    return lines
+
+def get_directional_lines(lines):
+    pass
 
 def area(coord1, coord2):
     return (1 + abs(coord1[0]-coord2[0])) * (1 + abs(coord1[1] - coord2[1]))
 
-def is_valid(coord1, coord2, coords):
-    y1, x1 = coord1
-    y2, x2 = coord2
-    
-    for coord in coords:
-        current_y, current_x = coord
-        if min(x1, x2) <= current_x <= max(x1, x2):
-            if min(y1, y2) <= current_y <= max(y1, y2):
-                if coord != coord1 and coord != coord2:
-                    return False
-            
-    return True
+def is_valid(coord1, coord2, h_ranges, v_ranges):
+    # check if all 4 boundary lines are valid
+    pass
 
 if __name__ == "__main__":
     main()
