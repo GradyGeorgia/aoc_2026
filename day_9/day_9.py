ddc09f490a4790b0cb56c5d738117ab223a16de9
Grady <grady.w.georgia@gmail.com>
2026-01-07 17:39:51 -0600

    unit tests

--- day_9/day_9.py
+++ day_9/day_9.py
@@ -1,5 +1,5 @@
-import numpy as np
 from enum import Enum, auto
+import unittest
 
 class Direction(Enum):
     UP = 0
@@ -22,9 +22,9 @@ def main():
     directional_lines = get_directional_lines(lines)
     h_ranges = get_h_ranges(directional_lines)
     
-    t_lines = coords_to_lines(transpose_coords(coords))
-    t_directional_lines = get_directional_lines(t_lines)
-    v_ranges = get_h_ranges(t_directional_lines)
+    # t_lines = coords_to_lines(transpose_coords(coords))
+    # t_directional_lines = get_directional_lines(t_lines)
+    # v_ranges = get_h_ranges(t_directional_lines)
 
     res = 0
     for i in range(len(coords)):
@@ -53,6 +53,8 @@ def get_h_ranges(directional_lines):
         # populate horizontal lines
         if direction == Direction.UP or direction == Direction.DOWN:
             y_coord = coord1[0]
+            if y_coord not in h_directional_points:
+                    h_directional_points[y_coord] = []
             h_directional_points[y_coord].append((min(coord1[1], coord2[1]), Direction.RIGHT))
             h_directional_points[y_coord].append((max(coord1[1], coord2[1]), Direction.LEFT))
             
@@ -60,7 +62,8 @@ def get_h_ranges(directional_lines):
     h_ranges = {} # h_ranges[y]->[(x1,x2)...]
     for y_coord in h_directional_points:
         directional_points = h_directional_points[y_coord]
-        directional_points.sort()
+        directional_points.sort(key=lambda p: (p[0]))
+        print(directional_points)
 
         # create initial ranges
         ranges = []
@@ -118,7 +121,7 @@ def get_directional_lines(lines):
     def rotates_clockwise(line1, line2):
         dir1 = get_direction(line1)
         dir2 = get_direction(line2)
-        if dir2.value > dir1.value or (dir1 == Direction.RIGHT and dir2 == Direction.UP):
+        if dir2.value > dir1.value or (dir1 == Direction.LEFT and dir2 == Direction.UP):
             return True
         return False
     
@@ -127,7 +130,7 @@ def get_directional_lines(lines):
     for i, line in enumerate(lines):
         # is it horizontal
         if line[0][0] == line[1][0]:
-            if line[0][0] <  lines[topmost][0]:
+            if line[0][0] <  lines[topmost][0][0]:
                 topmost = i
                 
     # topmost points in, compute directions sequentially    
@@ -140,6 +143,8 @@ def get_directional_lines(lines):
             new_direction = Direction((prev_direction.value + 1) % 4)
         else:
             new_direction = Direction((prev_direction.value - 1) % 4)
+
+        print(prev, next, new_direction)
         directional_lines.append(DirectionalLine(next[0], next[1], new_direction))
         
     return directional_lines
@@ -185,5 +190,60 @@ def is_valid(coord1, coord2, h_ranges, v_ranges):
     if not horizontal_boundary_is_valid(coord2, coord1, h_ranges):
         return False
     return True
+class Test(unittest.TestCase):
+    def test_directional_lines(self):
+        pass
+
+    def test_example_input(self):
+        input = '''7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3'''.strip()
+        coords = [[int(x) for x in line.split(",")] for line in input.split("\n")]
+        lines = coords_to_lines(coords)
+        directional_lines = get_directional_lines(lines)
+        h_ranges = get_h_ranges(directional_lines)
+        assert(0 not in h_ranges)
+        assert(h_ranges[1][0] == (7,11))
+        assert(h_ranges[2][0] == (7,11))
+        assert(h_ranges[3][0] == (2,11))
+        assert(h_ranges[4][0] == (2,11))
+        assert(h_ranges[5][0] == (2,11))
+        assert(h_ranges[6][0] == (9,11))
+        assert(h_ranges[7][0] == (9,11))
+        assert(8 not in h_ranges)
+
+    def test_example_input_vertical(self):
+        input = '''7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3'''.strip()
+        coords = [[int(x) for x in line.split(",")] for line in input.split("\n")]
+        t_lines = coords_to_lines(transpose_coords(coords))
+        t_directional_lines = get_directional_lines(t_lines)
+        v_ranges = get_h_ranges(t_directional_lines)
+        assert(0 not in v_ranges)
+        assert(1 not in v_ranges)
+        assert(v_ranges[2][0] == (3,5))
+        assert(v_ranges[3][0] == (3,5))
+        assert(v_ranges[4][0] == (3,5))
+        assert(v_ranges[5][0] == (3,5))
+        assert(v_ranges[6][0] == (3,5))
+        assert(v_ranges[7][0] == (1,5))
+        assert(v_ranges[8][0] == (1,5))
+        assert(v_ranges[9][0] == (1,7))
+        assert(v_ranges[10][0] == (1,7))
+        assert(v_ranges[11][0] == (1,7))
+        assert(12 not in v_ranges)
+
 if __name__ == "__main__":
-    main()
+    # main()
+    unittest.main()
